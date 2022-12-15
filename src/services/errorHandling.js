cd38e1cde34970a64a78f4855952e1d9afe7b93c
Mahmoud Elwan <mahmoudelwan460@gmail.com>
2022-12-15 02:01:27 +0200

    ji

--- src/services/errorHandling.js
+++ src/services/errorHandling.js
@@ -14,13 +14,13 @@ export function asyncHandler(fun) {
 export const globalErrorHandling = (err, req, res, next) => {
     if (err) {
         if (process.env.MOOD === 'DEV') {
-            return res.status(err['cause']).json({
+            return res.status(err['cause'] || 500).json({
                 message: "error",
                 err: err.message,
                 stack: err.stack
             })
         } else {
-            return res.status(err['cause']).json({ message: "error", err: err.message, stack: err.stack })
+            return res.status(err['cause'] || 500).json({ message: "error", err: err.message, stack: err.stack })
         }
     }
 
